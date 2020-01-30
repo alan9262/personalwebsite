2d54a22de76b8ce86559f08c84864e71971e7876
alan9262 <alankrit.chaturvedi@gmail.com>
2020-01-30 04:02:19 -0500

    Updates

---
+++ precache-manifest.a5231cfdcf46741e56874ac247f081e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf88975888e49210b4eb",
+    "url": "/static/css/main.71f5719b.chunk.css"
+  },
+  {
+    "revision": "cf88975888e49210b4eb",
+    "url": "/static/js/main.cf889758.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "6bcdb61dd4f3ca8be192e661fd84d3d7",
+    "url": "/index.html"
+  }
+];
